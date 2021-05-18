75a4160acdf3453883c8aeba1d172d070df3c9fe
mrWeiss <giorgio2197@gmail.com>
2021-05-18 13:51:23 +0200

    add ExamEdit filter

--- src/it/polimi/poliesami/view/ProfEditExamPage.java
+++ src/it/polimi/poliesami/view/ProfEditExamPage.java
@@ -12,10 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
-import it.polimi.db.business.ExamBean;
-import it.polimi.db.business.ExamRegistrationBean;
 import it.polimi.db.business.ExamResult;
-import it.polimi.db.dao.ExamRegistrationDAO;
 
 public class ProfEditExamPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -31,17 +28,8 @@ public class ProfEditExamPage extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		final ServletContext servletCtx = getServletContext();
-		final ExamBean exam = (ExamBean) req.getAttribute("exam");
-		final int examId = exam.getId();
-
-		final String studentIdString = req.getParameter("studentId");
-		final int studentId = Integer.parseInt(studentIdString);
-
-		final ExamRegistrationDAO examRegistrationDAO = (ExamRegistrationDAO) servletCtx.getAttribute("examRegistrationDAO");
-		final ExamRegistrationBean examRegistration = examRegistrationDAO.getProfessorExamRegistration(studentId, examId);
 
 		final WebContext ctx = new WebContext(req, res, servletCtx, req.getLocale());
-		ctx.setVariable("examRegistration", examRegistration);
 		ctx.setVariable("results", ExamResult.values());
 
 		final TemplateEngine templateEngine = (TemplateEngine) servletCtx.getAttribute("templateEngine");
