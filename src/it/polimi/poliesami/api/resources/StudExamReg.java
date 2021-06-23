4941f66da6ded5e398e1ecbccca0b1d62426ff20
Guido Rolle <rolleguido@gmail.com>
2021-06-23 18:14:50 +0200

    Add StudExamReg doPost

--- src/it/polimi/poliesami/api/resources/StudExamReg.java
+++ src/it/polimi/poliesami/api/resources/StudExamReg.java
@@ -17,6 +17,46 @@ import it.polimi.poliesami.db.dao.ExamRegistrationDAO;
 import it.polimi.poliesami.api.NullableData;
 
 public class StudExamReg extends HttpServlet {
+
+	public enum ACTION {
+		REGISTER("register") {
+			@Override
+			public boolean execute(ExamRegistrationDAO dao, int careerId, int examId) {
+				return dao.registerToExam(careerId, examId);
+			}
+		},
+		DEREGISTER("deregister") {
+			@Override
+			public boolean execute(ExamRegistrationDAO dao, int careerId, int examId) {
+				return dao.deregisterFromExam(careerId, examId);
+			}
+		},
+		REJECT("reject") {
+			@Override
+			public boolean execute(ExamRegistrationDAO dao, int careerId, int examId) {
+				return dao.rejectStudExam(careerId, examId);
+			}
+		};
+
+		private String string;
+
+		private ACTION(String action){ this.string = action; }
+
+		@Override
+		public String toString(){ return this.string; }
+
+		public static ACTION fromString(String action) {
+			for(ACTION actionType : ACTION.values()) {
+				if(actionType.string.equalsIgnoreCase(action)) {
+					return actionType;
+				}
+			}
+			return null;
+		}
+
+		public abstract boolean execute(ExamRegistrationDAO dao, int careerId, int examId);
+	}
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		final ServletContext servletCtx = getServletContext();
@@ -27,6 +67,29 @@ public class StudExamReg extends HttpServlet {
 		final ExamRegistrationBean examRegistration = examRegistrationDAO.getStudentExamRegistration(career.getId(), exam.getId());
 		
 		request.setAttribute("jsonBody", new NullableData(examRegistration));		
+		RequestDispatcher jsonDispatcher = getServletContext().getNamedDispatcher("JsonMapper");
+		jsonDispatcher.forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		ServletContext servletCtx = getServletContext();
+		final CareerBean career = (CareerBean) request.getAttribute("career");
+		final ExamBean exam = (ExamBean) request.getAttribute("exam");
+		
+		final ACTION action = ACTION.fromString(request.getParameter("action"));
+		if(action == null) {
+			request.setAttribute("jsonError", new Exception("Invalid action"));
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			RequestDispatcher jsonDispatcher = getServletContext().getNamedDispatcher("JsonMapper");
+			jsonDispatcher.forward(request, response);
+			return;
+		}
+
+		final ExamRegistrationDAO examRegistrationDAO = (ExamRegistrationDAO) servletCtx.getAttribute("examRegistrationDAO");
+		final boolean result = action.execute(examRegistrationDAO, career.getId(), exam.getId());
+		
+		request.setAttribute("jsonBody", result);
 		RequestDispatcher jsonDispatcher = getServletContext().getNamedDispatcher("JsonMapper");
 		jsonDispatcher.forward(request, response);
 	}
