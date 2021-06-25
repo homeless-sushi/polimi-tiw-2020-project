5a2a28d0a36a03b7f87a9c5c607355eded6f89d0
Guido Rolle <rolleguido@gmail.com>
2021-06-25 17:28:25 +0200

    Add Api ProfExam resource

---
+++ src/it/polimi/poliesami/api/resources/ProfExam.java
@@ -0,0 +1,22 @@
+package it.polimi.poliesami.api.resources;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.polimi.poliesami.db.business.ExamBean;
+
+public class ProfExam extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		final ExamBean exam = (ExamBean) request.getAttribute("exam");
+		
+		request.setAttribute("jsonBody", exam);		
+		RequestDispatcher jsonDispatcher = getServletContext().getNamedDispatcher("JsonMapper");
+		jsonDispatcher.forward(request, response);
+	}
+}
