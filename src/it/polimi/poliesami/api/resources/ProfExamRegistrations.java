e8f9492f324f5c31a4cb2906213a80955e359b4f
Guido Rolle <rolleguido@gmail.com>
2021-06-30 11:20:17 +0200

    Add Api ProfExamRegistrations doPost

--- src/it/polimi/poliesami/api/resources/ProfExamRegistrations.java
+++ src/it/polimi/poliesami/api/resources/ProfExamRegistrations.java
@@ -16,6 +16,39 @@ import it.polimi.poliesami.db.business.ExamRegistrationBean;
 import it.polimi.poliesami.db.dao.ExamRegistrationDAO;
 
 public class ProfExamRegistrations extends HttpServlet {
+	public enum ACTION {
+		PUBLISH("publish") {
+			@Override
+			public boolean execute(ExamRegistrationDAO dao, int examId) {
+				return dao.publishExamEval(examId);
+			}
+		},
+		VERBALIZE("verbalize") {
+			@Override
+			public boolean execute(ExamRegistrationDAO dao, int examId) {
+				return dao.verbalizeExamEval(examId);
+			}
+		};
+
+		private ACTION(String action){ this.string = action; }
+	
+		private String string;
+
+		@Override
+		public String toString(){ return this.string; }
+
+		public static ACTION fromString(String action) {
+			for(ACTION actionType : ACTION.values()) {
+				if(actionType.string.equalsIgnoreCase(action)) {
+					return actionType;
+				}
+			}
+			return null;
+		}
+
+		public abstract boolean execute(ExamRegistrationDAO dao, int examId);
+	}
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		final ServletContext servletCtx = getServletContext();
@@ -27,6 +60,29 @@ public class ProfExamRegistrations extends HttpServlet {
 		
 		request.setAttribute("jsonBody", new NullableData(registrations));
 		final RequestDispatcher jsonDispatcher = getServletContext().getNamedDispatcher("JsonMapper");
+		jsonDispatcher.forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException {
+		final ServletContext servletCtx = getServletContext();
+		final ExamBean exam = (ExamBean) request.getAttribute("exam");
+
+		final ACTION action = ACTION.fromString(request.getParameter("action"));
+		if(action == null) {
+			request.setAttribute("jsonError", new Exception("Invalid action"));
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			RequestDispatcher jsonDispatcher = getServletContext().getNamedDispatcher("JsonMapper");
+			jsonDispatcher.forward(request, response);
+			return;
+		}
+
+		final ExamRegistrationDAO examRegistrationDAO = (ExamRegistrationDAO) servletCtx.getAttribute("examRegistrationDAO");
+		final boolean result = action.execute(examRegistrationDAO, exam.getId());
+
+		request.setAttribute("jsonBody", result);
+		RequestDispatcher jsonDispatcher = getServletContext().getNamedDispatcher("JsonMapper");
 		jsonDispatcher.forward(request, response);
 	}
 }
